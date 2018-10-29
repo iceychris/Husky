be6197f05591c8000f60cfb482c9154ef5e25e8c
iceychris <christiansvde@freenet.de>
2018-10-29 01:55:55 +0000

    * calculate dft

--- Husky.hs
+++ Husky.hs
@@ -2,21 +2,33 @@
 
 module Main where
 
+-- builtins
 import System.IO
 import Data.Maybe
 import Data.Int
+import Data.Complex
+import GHC.Float
+
+-- 3rd party
 import Data.ByteString.Internal as BS
 import qualified Data.Vector.Storable as V
-
 import Sound.Pulse.Simple
 import Numeric.FFT.Vector.Plan
 import Numeric.FFT.Vector.Unitary
+import qualified Numeric.FFT.Vector.Invertible as I
 
 
 -- TODO
 -- - add signal handler to call simpleFree at some point... 
--- - make capture take a function like (B.ByteString -> IO a)
---
+
+
+-- constants
+samplerate = 44100
+bufferchunk = 4096 -- 1024
+transform = I.dftR2C
+myplan = plan transform (bufferchunk :: Int)
+
+
 
 
 -- make calculations on data possible
@@ -24,10 +36,8 @@ bytesToFloats :: BS.ByteString -> V.Vector Float
 bytesToFloats = V.unsafeCast . aux . BS.toForeignPtr
   where aux (fp,offset,len) = V.unsafeFromForeignPtr fp offset len
 
-samplerate = 44100
-bufferchunk = 4096 -- 1024
---transform = Transform 
-
+toDouble :: V.Vector Float -> V.Vector Double
+toDouble vec = V.map (\x -> float2Double x) vec
 
 readSample :: Simple -> IO ByteString 
 readSample s = simpleReadRaw s bufferchunk :: IO ByteString 
@@ -44,36 +54,46 @@ putStrLnFloat :: ByteString -> IO ()
 putStrLnFloat bytes = do
     System.IO.putStrLn $ show $ V.maximum $ vecAbs $ bytesToFloats bytes
 
-
-volMaxChars = 40
-volume :: ByteString -> IO ()
-volume bs = do
+-- value, maximum
+vbar :: Float -> Int -> IO ()
+vbar val maxi = do
     putStr $ replicate n '█'
     putStrLn "▓▒░"
     where
-        floatmax = V.maximum $ vecAbs $ bytesToFloats bs
-        n = round $ floatmax * volMaxChars
+        n = round $ (*) val $ fromIntegral maxi
 
--- fft :: Transform a b -> ByteString -> IO ()
--- fft = putStrLn "dummy"
--- 
--- getPlan :: Simple -> Maybe Transform a b 
--- getPlan = Nothing 
+
+fft :: V.Vector Double -> V.Vector Double
+fft floats = V.map magnitude resfft
+    where 
+        resfft = execute myplan floats
+
+volMaxChars = 40
+volume :: ByteString -> IO ()
+volume bs = vbar fmax volMaxChars 
+    where
+        fmax = V.maximum $ vecAbs $ bytesToFloats bs
+
+maxfft = 40
+volumefft :: ByteString -> IO ()
+volumefft bs = vbar headFloats maxfft
+    where
+        doubles = (.) toDouble bytesToFloats bs
+        headFloats = double2Float $ V.head doubles
 
 
 -- loop forever reading audio :)
 capture :: Simple -> Float -> IO ()
 capture s prevVol = do
     -- (readSample s) >>= putStrLnFloat
-    (readSample s) >>= volume
+    -- (readSample s) >>= volume
+    (readSample s) >>= volumefft
     capture s prevVol
 
 
 main = do
     s <- simpleNew Nothing title Record Nothing description 
       (SampleSpec (F32 LittleEndian) samplerate 1) Nothing bufattr
-    -- plan <- planOfType Measure 
-    --plan <- getPlan s 
     capture s 1.0
     simpleFree s
     where
