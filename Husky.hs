9864563286daf2c6d7826a753c4c4868af4f4464
iceychris <christiansvde@freenet.de>
2018-11-02 02:50:26 +0000

    + vty output lib

--- Husky.hs
+++ Husky.hs
@@ -3,6 +3,9 @@
 module Main where
 
 -- builtins
+import Control.Applicative hiding ((<|>))
+import Control.Arrow
+--import Control.Monad.RWS
 import System.IO
 import Data.Maybe
 import Data.Int
@@ -17,6 +20,12 @@ import Numeric.FFT.Vector.Plan
 import Numeric.FFT.Vector.Unitary
 import qualified Numeric.FFT.Vector.Invertible as I
 
+import Graphics.Vty
+import Graphics.Vty.Attributes
+import Graphics.Vty.Inline
+import Graphics.Vty.Picture
+import Graphics.Vty.Output
+
 
 -- TODO
 -- - add signal handler to call simpleFree at some point... 
@@ -25,11 +34,31 @@ import qualified Numeric.FFT.Vector.Invertible as I
 -- constants
 samplerate = 44100
 bufferchunk = 4096 -- 1024
+fftinput = (fst (bufferchunk `divMod` 4)) :: Int
 transform = I.dftR2C
-myplan = plan transform (bufferchunk :: Int)
+myplan = plan transform fftinput 
+
+volMaxChars = 0.1
+ab = 0.1
+barWidth = 1 -- for now only 1 works?
+binsToTake = 5
+
+-- old
+maxfft = 40
 
 
-
+foldt            :: (a -> a -> a) -> a -> [a] -> a
+foldt f z []     = z
+foldt f z [x]    = x
+foldt f z xs     = foldt f z (pairs f xs)
+ 
+foldi            :: (a -> a -> a) -> a -> [a] -> a
+foldi f z []     = z
+foldi f z (x:xs) = f x (foldi f z (pairs f xs))
+ 
+pairs            :: (a -> a -> a) -> [a] -> [a]
+pairs f (x:y:t)  = f x y : pairs f t
+pairs f t        = t
 
 -- make calculations on data possible
 bytesToFloats :: BS.ByteString -> V.Vector Float
@@ -54,13 +83,50 @@ putStrLnFloat :: ByteString -> IO ()
 putStrLnFloat bytes = do
     System.IO.putStrLn $ show $ V.maximum $ vecAbs $ bytesToFloats bytes
 
--- value, maximum
-vbar :: Float -> Int -> IO ()
-vbar val maxi = do
-    putStr $ replicate n '█'
-    putStrLn "▓▒░"
+-- 
+maxBarLen = 30
+bar :: Int -> String
+bar n = a ++ b ++ replicate (maxBarLen-n) ' '
     where
-        n = round $ (*) val $ fromIntegral maxi
+        a = replicate n '█'
+        b = "▓▒░"
+
+-- value, maximum
+vbar :: Float -> Float -> IO ()
+vbar val maxi = putStrLn $ bar $ displayable val maxi
+
+
+-- convert a float with a maximum to an int
+-- ?
+displayable :: Float -> Float -> Int
+displayable val maxi = round $ (*) val $ maxi
+
+
+
+-- vertically concatenated bar, e.g.
+-- ░░░
+-- ▒▒▒
+-- ▓▓▓
+-- ███
+-- ███
+-- ███
+imbar :: Int -> Int -> Image
+imbar n width = vertCat $ replicate width $ string defAttr $ bar n 
+
+colorbox_16 :: Image
+colorbox_16 = border <|> column0 <|> border <|> column1 <|> border <|> column2 <|> border
+    where
+        column0 = vertCat $ map lineWithColor normal
+        column1 = vertCat $ map lineWithColor bright
+        border = vertCat $ replicate (length normal) $ string defAttr " | "
+        column2 = vertCat $ map (string defAttr . snd) normal
+        lineWithColor (c, cName) = string (defAttr `withForeColor` c) cName
+        normal = zip [ black, red, green, yellow, blue, magenta, cyan, white ]
+                     [ "black", "red", "green", "yellow", "blue", "magenta", "cyan", "white" ]
+        bright = zip [ brightBlack, brightRed, brightGreen, brightYellow, brightBlue
+                     , brightMagenta, brightCyan, brightWhite ]
+                     [ "bright black", "bright red", "bright green", "bright yellow"
+                     , "bright blue", "bright magenta", "bright cyan", "bright white" ]
 
 
 fft :: V.Vector Double -> V.Vector Double
@@ -68,13 +134,11 @@ fft floats = V.map magnitude resfft
     where 
         resfft = execute myplan floats
 
-volMaxChars = 40
 volume :: ByteString -> IO ()
 volume bs = vbar fmax volMaxChars 
     where
         fmax = V.maximum $ vecAbs $ bytesToFloats bs
 
-maxfft = 40
 volumefft :: ByteString -> IO ()
 volumefft bs = vbar headFloats maxfft
     where
@@ -82,19 +146,74 @@ volumefft bs = vbar headFloats maxfft
         headFloats = double2Float $ V.head doubles
 
 
+volumefft2 :: Int -> IO ()
+volumefft2 val = do
+    t <- standardIOConfig >>= outputForConfig
+    reserveDisplay t
+    (w,h) <- displayBounds t
+    let row0 = replicate (fromEnum w) 'X' ++ "\n"
+        rowH = replicate (fromEnum w - 1) 'X'
+        rowN = "X" ++ replicate (fromEnum w - 2) ' ' ++ "X\n"
+        image = row0 ++ 
+            (concat $ replicate (fromEnum (fst (h `divMod` 2)) - 2) rowN) ++ 
+            (bar val) ++ "\n" ++
+            (concat $ replicate (fromEnum (fst (h `divMod` 2)) - 2) rowN) ++ 
+            rowH
+    putStr image
+    hFlush stdout
+    -- releaseDisplay t
+    -- releaseTerminal t
+    return ()
+
+
+valToImage :: Double -> Image
+valToImage val = imbar (displayable (double2Float val) volMaxChars) barWidth
+
+
+volumefft3 :: V.Vector Float -> Image 
+volumefft3 vec = do
+    -- [Image] -> Image
+    -- using <|>
+    --  which is
+    --   Image -> Image -> Image
+    (foldt (\a b -> a <|> b) (head images) (tail images)) <|> colorbox_16
+    -- vertCat images
+    where
+        doubles = toDouble vec
+        ffts = fft doubles
+        slice = V.take binsToTake ffts
+        lslice = V.toList slice
+        images = map (\val -> valToImage val) lslice
+
+
+
+animate :: Vty -> ByteString -> IO ()
+animate vty bs = do
+    let pic = picForImage $ volumefft3 floats  
+    update vty pic
+    -- volumefft2 $ (round $ 40.0 * desired :: Int)
+    where
+        floats = bytesToFloats bs
+    --     desired = V.maximum floats
+
+
 -- loop forever reading audio :)
-capture :: Simple -> Float -> IO ()
-capture s prevVol = do
+capture :: Vty -> Simple -> Float -> IO ()
+capture vty s prevVol = do
     -- (readSample s) >>= putStrLnFloat
     -- (readSample s) >>= volume
-    (readSample s) >>= volumefft
-    capture s prevVol
+    -- (readSample s) >>= volumefft
+    (readSample s) >>= (animate vty)
+    capture vty s prevVol
+
 
 
 main = do
     s <- simpleNew Nothing title Record Nothing description 
       (SampleSpec (F32 LittleEndian) samplerate 1) Nothing bufattr
-    capture s 1.0
+    -- create vty
+    vty <- mkVty defaultConfig
+    capture vty s 1.0
     simpleFree s
     where
         title = "Husky"
