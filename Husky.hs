3679e434d2c6fa2fe01a8d548a7e9f408080ef6d
iceychris <christiansvde@freenet.de>
2018-11-02 23:56:31 +0000

    + Util.hs + multi-thread + centered image output + quit on 'Ctrl-C' or 'q'

--- Husky.hs
+++ Husky.hs
@@ -7,10 +7,13 @@ import Control.Applicative hiding ((<|>))
 import Control.Arrow
 --import Control.Monad.RWS
 import System.IO
+import System.Exit
 import Data.Maybe
 import Data.Int
 import Data.Complex
 import GHC.Float
+import Control.Concurrent (forkIO)
+import Control.Concurrent.MVar (newEmptyMVar, tryTakeMVar, putMVar, MVar)
 
 -- 3rd party
 import Data.ByteString.Internal as BS
@@ -26,46 +29,46 @@ import Graphics.Vty.Inline
 import Graphics.Vty.Picture
 import Graphics.Vty.Output
 
+-- local libs
+import Util
+
 
 -- TODO
--- - add signal handler to call simpleFree at some point... 
--- - make terminating via Ctrl-C possible
--- - remove charsFade when input value < 3
+-- OK make terminating via Ctrl-C possible
+    -- OK (1) start vty watcher thread
+        -- OK use nextEvent to get a new event in a blocking fashion
+    -- OK (2) check for MVar 
+-- remove charsFade when input value < 3
+-- fix output dimensions
+    -- (3) get terminal size 
 
 
 -- constants
 samplerate = 44100
-bufferchunk = 1024 -- 1024
+bufferchunk = 4096 -- 1024
 fftinput = (fst (bufferchunk `divMod` 4)) :: Int
 transform = I.dftR2C
 myplan = plan transform fftinput 
 
-volMaxChars = 0.07
-ab = 0.1
+volMaxChars = 0.3
 barWidth = 2 -- for now only 1 works?
-binsToTake = 64
+binsToTake = 60
 -- charsFill = '#' -- '█'
 charsFill = '█'
 -- charsFade = "|*." --"▓▒░"
 charsFade = "▓▒░"
+-- charsEmpty = '0'
+charsEmpty = ' '
+maxBarLen = 15 -- height
 
+title = "Husky"
+description = "Audio Visualizer"
+bufattr = Just $ BufferAttr Nothing Nothing Nothing Nothing $ Just samplerate 
 
 -- old
 maxfft = 40
 
 
-foldt            :: (a -> a -> a) -> a -> [a] -> a
-foldt f z []     = z
-foldt f z [x]    = x
-foldt f z xs     = foldt f z (pairs f xs)
- 
-foldi            :: (a -> a -> a) -> a -> [a] -> a
-foldi f z []     = z
-foldi f z (x:xs) = f x (foldi f z (pairs f xs))
- 
-pairs            :: (a -> a -> a) -> [a] -> [a]
-pairs f (x:y:t)  = f x y : pairs f t
-pairs f t        = t
 
 -- make calculations on data possible
 bytesToFloats :: BS.ByteString -> V.Vector Float
@@ -91,12 +94,15 @@ putStrLnFloat bytes = do
     System.IO.putStrLn $ show $ V.maximum $ vecAbs $ bytesToFloats bytes
 
 -- 
-maxBarLen = 30
 bar :: Int -> String
-bar n = a ++ b ++ replicate (maxBarLen-n) ' '
+bar n | n < 3  = chF ++ (replicate (maxBarLen-n) charsEmpty)
+      | n >= 3 && n <= maxBarLen = filled ++ charsFade ++ replicate (maxBarLen-n) charsEmpty 
+      | n > maxBarLen = replicate maxBarLen charsFill
     where
-        a = replicate n charsFill
-        b = charsFade
+        chF = reverse $ take n $ reverse charsFade
+        filled = replicate (n-3) charsFill
+
+
 
 --
 strBar :: Int -> [String]
@@ -106,11 +112,6 @@ strBar n = map (\c -> [c]) (bar n)
 vbar :: Float -> Float -> IO ()
 vbar val maxi = putStrLn $ bar $ displayable val maxi
 
-
--- convert a float with a maximum to an int
--- ?
-displayable :: Float -> Float -> Int
-displayable val maxi = round $ (*) val $ maxi
 
 
 
@@ -169,16 +170,14 @@ volumefft2 val = do
 valToImage :: Double -> Image
 valToImage val = imbar (displayable (double2Float val) volMaxChars) barWidth
 
+mline :: Int -> Char -> Image
+mline wdh c = string defAttr (replicate wdh c)
 
 volumefft3 :: V.Vector Float -> Image 
 volumefft3 vec = do
-    -- [Image] -> Image
-    -- using <|>
-    --  which is
-    --   Image -> Image -> Image
-    (foldt (\a b -> a <|> b) (head images) (tail images))
-    -- vertCat images
+    foldt (\a b -> a <|> b) (head images) (tail images)
     where
+        wdh = binsToTake * barWidth
         doubles = toDouble vec
         ffts = fft doubles
         slice = V.take binsToTake ffts
@@ -186,36 +185,91 @@ volumefft3 vec = do
         images = map (\val -> valToImage val) lslice
 
 
+centerRect :: (Int, Int) -> Image -> Image
+centerRect (w, h) img = translate tx ty img
+    where
+        tx = fst $ (w - (binsToTake * barWidth)) `divMod` 2 
+        ty = fst $ (h - maxBarLen) `divMod` 2
+
 
 animate :: Vty -> ByteString -> IO ()
 animate vty bs = do
-    let pic = picForImage $ volumefft3 floats  
-    update vty pic
-    -- volumefft2 $ (round $ 40.0 * desired :: Int)
+    region <- displayBounds $ outputIface vty
+    update vty $ picForImage (centerRect region (vertJoin fftoutput qline))
     where
         floats = bytesToFloats bs
-    --     desired = V.maximum floats
+        fftoutput = volumefft3 floats
+        c = (binsToTake*barWidth)
+        qline = mline c charsFill
 
+
+
+handleIOEvent :: IO Event -> IO ()
+handleIOEvent x = do
+    return () 
 
 -- loop forever reading audio :)
-capture :: Vty -> Simple -> Float -> IO ()
-capture vty s prevVol = do
-    -- (readSample s) >>= putStrLnFloat
-    -- (readSample s) >>= volume
-    -- (readSample s) >>= volumefft
-    (readSample s) >>= (animate vty)
-    capture vty s prevVol
+capture :: MVar Event -> Vty -> Simple -> Float -> IO ()
+capture ioBox vty s prevVol = do
+    -- (3) get terminal size 
+    -- (2) check for MVar 
+    -- ev :: Maybe (IO Event)
+    ev <- tryTakeMVar ioBox
+    case ev of 
+        Nothing -> do
+            -- handleIOEvent x
+            (readSample s) >>= (animate vty)
+            capture ioBox vty s prevVol
+        Just x -> do
+            -- free and return
+            if shouldAbort x 
+                then gracefulShutdown vty s
+                else capture ioBox vty s prevVol
+
+gracefulShutdown :: Vty -> Simple -> IO ()
+gracefulShutdown vty s = do
+    shutdown vty
+    simpleFree s
+
+
+shouldAbort :: Event -> Bool
+shouldAbort ev = case ev of 
+    (EvKey (KChar 'c') [MCtrl]) -> True
+    (EvKey (KChar 'q') _) -> True
+    _ -> False
+
+
+watchForIOEvents :: MVar Event -> Vty -> IO ()
+watchForIOEvents ioBox vty = do
+    ev <- nextEvent vty
+    putStrLn $ show ev
+
+    -- sent over to other thread
+    putMVar ioBox $ ev 
+
+    -- pattern match for Ctrl-C 
+    if shouldAbort ev
+        then exitWith ExitSuccess
+        else watchForIOEvents ioBox vty
 
 
 
 main = do
+
+    -- communication between UI and recorder thread
+    ioBox <- newEmptyMVar
+
+    -- simple pulse object
     s <- simpleNew Nothing title Record Nothing description 
       (SampleSpec (F32 LittleEndian) samplerate 1) Nothing bufattr
+
     -- create vty
     vty <- mkVty defaultConfig
-    capture vty s 1.0
-    simpleFree s
-    where
-        title = "Husky"
-        description = "Audio Visualizer"
-        bufattr = Just $ BufferAttr Nothing Nothing Nothing Nothing $ Just samplerate 
+
+    -- (1) start vty watcher thread
+    forkIO $ capture ioBox vty s 1.0
+
+    -- watch for IO Events
+    watchForIOEvents ioBox vty
+
+
---
+++ Util.hs
@@ -0,0 +1,23 @@
+module Util where
+
+-- foldt implementation taken from
+--  https://wiki.haskell.org/Fold 
+foldt            :: (a -> a -> a) -> a -> [a] -> a
+foldt f z []     = z
+foldt f z [x]    = x
+foldt f z xs     = foldt f z (pairs f xs)
+ 
+foldi            :: (a -> a -> a) -> a -> [a] -> a
+foldi f z []     = z
+foldi f z (x:xs) = f x (foldi f z (pairs f xs))
+ 
+pairs            :: (a -> a -> a) -> [a] -> [a]
+pairs f (x:y:t)  = f x y : pairs f t
+pairs f t        = t
+
+
+
+-- convert a float with a maximum to an int
+-- ?
+displayable :: Float -> Float -> Int
+displayable val maxi = round $ (*) val $ maxi
